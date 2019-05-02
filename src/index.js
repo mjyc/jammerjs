bcd6eaae908c3b5f0aa03f97cc8af2503cc756be
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-05-02 11:00:34 -0700

    WIP: hack hack hack

--- src/index.js
+++ src/index.js
@@ -1,33 +1,8 @@
-// require('../node_modules/handtrackjs/dist/handtrack.min.js');
+const handtrack = require('../node_modules/handtrackjs/dist/handtrack.min.js');
+require('../node_modules/hammer-simulator/index.js');
 
-// console.log('handTrack', handtrack);
-
-// console.log(require('hammerjs'));
-console.log(require('../node_modules/handtrackjs/dist/handtrack.min.js'));
-// import * as handTrack from 'handtrackjs';
-// console.log(handTrack);
-
-// console.log(require('hammer-simulator'));
-
-// console.log(Simulator);
-
-
-// Simulator.setType('touch');
-// Simulator.events.touch.fakeSupport();
-
-
-
-
-// class HandtrackInput extends Hammer.Input {
-//   constructor() {
-//     super(...arguments);
-//     console.log('created!');
-//   }
-
-//   handler(ev) {
-//     console.log('handler');
-//   }
-// }
+console.log('handTrack', handtrack);
+console.log(Simulator);
 
 export default function HandtrackTouch () {
   return 'HandtrackTouch';
