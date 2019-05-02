f3d4176cfd2936cc1f09b7a33472689335f59b46
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2019-05-02 09:53:15 -0700

    WIP: integrate simulator

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 console.log('hello world!');
-console.log(require('hammerjs'));
-console.log(require('../node_modules/handtrackjs/dist/handtrack.min.js'));
+// console.log(require('hammerjs'));
+// console.log(require('../node_modules/handtrackjs/dist/handtrack.min.js'));
 // import * as handTrack from 'handtrackjs';
 // console.log(handTrack);
+
+// console.log(require('hammer-simulator'));
+
+// console.log(Simulator);
+
+
+// Simulator.setType('touch');
+// Simulator.events.touch.fakeSupport();
+
+
+
+
+// class HandtrackInput extends Hammer.Input {
+//   constructor() {
+//     super(...arguments);
+//     console.log('created!');
+//   }
+
+//   handler(ev) {
+//     console.log('handler');
+//   }
+// }
 
 export default function TimeTravel () {
   return 'TimeTravel';
